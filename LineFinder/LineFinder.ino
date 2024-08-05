421c577669764ddd69c09dda3a3631868741f030
cameronly08 <cameron.ly@education.nsw.gov.au>
2024-08-05 12:15:54 +1000

    create ultrasonic sensor docs

--- LineFinder/LineFinder.ino
+++ LineFinder/LineFinder.ino
@@ -9,7 +9,6 @@ LineFinder lineFinder(LEFT_SENSOR_PIN, RIGHT_SENSOR_PIN);
 void setup() {
   lineFinder.init(); // Initialize the line finder sensors
 }
-
 void loop() {
  // for later -> lineFinder.followLine(DDR); // Follow the line using the motor controller
 }
